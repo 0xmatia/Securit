b475541a91e0d110f68bab740d51848fe7d72c2b
Elad Matia <elad.matia@gmail.com>
2019-11-14 21:58:43 -0500

    Now the module can retrieve username of password

--- Simple_Pam/pampam.c
+++ Simple_Pam/pampam.c
@@ -1,11 +1,25 @@
 #include <security/pam_modules.h>
 #include <security/pam_appl.h>
 #include <stdio.h>
+#include <string.h>
 
 #define PAM_SM_AUTH
+#define CONFIG_LOCATION "/etc/pasten.conf" //location of the user data
 
 PAM_EXTERN int pam_sm_authenticate(pam_handle_t *pamh, int flags, int argc, const char **argv)
 {
-    printf("Thank me later boi\n");
+    char* username = "";
+    char* password = "Hey";
+
+    pam_get_user(pamh, &username, NULL);
+    pam_get_authtok(pamh, PAM_AUTHTOK, &password , NULL);
+    
+    if (password == NULL || username == NULL)
+        return PAM_CRED_ERR;
+
+
+    
+    printf("%s\n", password);
+    printf("%s\n", username);
     return PAM_SUCCESS;
 }
