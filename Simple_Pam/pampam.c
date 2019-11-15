b7888648323c088578b648b74e3c41c28c5b74a6
Elad Matia <elad.matia@gmail.com>
2019-11-15 17:31:37 -0500

    Finished the test project: now I can parse dynamically usernames and passwords from a file (in json format). Please note: I used the "JSMN" json library to help me parse the json

--- Simple_Pam/pampam.c
+++ Simple_Pam/pampam.c
@@ -5,6 +5,9 @@
 
 #define PAM_SM_AUTH
 #define CONFIG_LOCATION "/etc/pasten.conf" //location of the user data
+#define CONFIG_FILE_MAX_SIZE 4096
+
+int verifyCreds(char** username, char** password);
 
 PAM_EXTERN int pam_sm_authenticate(pam_handle_t *pamh, int flags, int argc, const char **argv)
 {
@@ -21,5 +24,20 @@ PAM_EXTERN int pam_sm_authenticate(pam_handle_t *pamh, int flags, int argc, cons
     
     printf("%s\n", password);
     printf("%s\n", username);
+    return verifyCreds(&username, &password);
+}
+
+int verifyCreds(char** username, char** password)
+{
+    //Read the file first:
+    FILE* dataP = fopen(CONFIG_LOCATION, 'r');
+    char data[CONFIG_FILE_MAX_SIZE] = 0;
+    int fileSize = 0;
+    if(!dataP)
+        return PAM_CRED_ERR;
+    //Get the file size
+
+
+    fclose(dataP); //close file only if opened 
     return PAM_SUCCESS;
 }
